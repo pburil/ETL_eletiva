0c3fb5f9f55bb9024783321aeb74f8fc2566fa5f
pburil <61031157+pburil@users.noreply.github.com>
2021-04-17 16:03:37 -0300

    Add files via upload

---
+++ Estrutura condicional.R
@@ -0,0 +1,9 @@
+
+#Estrutura condicional
+
+Sport <- 1 # Number of goals scored by Team A
+SantaCruz <- 3 # Number of goals scored by Team B
+if (SantaCruz > Sport){
+  print ("Santa Cruz maior de Pernambuco")
+}
+
