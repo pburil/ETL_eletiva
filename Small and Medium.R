2cf1825e0c88d65ca0e67b6ec21c9ac94814cc5e
pburil <61031157+pburil@users.noreply.github.com>
2021-05-16 19:50:02 -0300

    Update Small and Medium.R

--- Small and Medium.R
+++ Small and Medium.R
@@ -2,18 +2,21 @@ library(data.table)
 
 vdemEndereco <- 'https://www.v-dem.net/en/data/data/v-dem-dataset-v111/#'
 
-# extração direta via read.csv
+# extraÃ§Ã£o direta via read.csv
 system.time(extracao1 <- read.csv2(vdemEndereco))
 
-# extração via amostragem com read.csv
+# extraÃ§Ã£o via amostragem com read.csv
 
 # ler apenas as primeiras 20 linhas
 amostra1 <- read.csv2(vdemEndereco , nrows=20)  
 
 amostra1Classes <- sapply(amostra1, class) 
 
-# fazemos a leitura passando as classes de antemão, a partir da amostra
+# fazemos a leitura passando as classes de antemÃ£o, a partir da amostra
 system.time(extracao2 <- data.frame(read.csv2("https://www.v-dem.net/en/data/data/v-dem-dataset-v111/#", colClasses=amostra1Classes) ) )  
 
-# extração via função fread, que já faz amostragem automaticamente
+# extraÃ§Ã£o via funÃ§Ã£o fread, que jÃ¡ faz amostragem automaticamente
 system.time(extracao3 <- fread(vdemEndereco))
+
+
+## A funÃ§Ã£o fread se mostrou mais eficiente em quase 3 segundos. 
